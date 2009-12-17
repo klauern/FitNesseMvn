bb3661427e57b2b8d4c53dacbbe29c6cf8bd22b0
Nick Klauer <klauer@gmail.com>
2009-12-16 23:28:19 -0600

    Created initial Git repo to try to get fitnesse to work with Maven properly.

---
+++ src/main/java/klauer/fitnessemvn/App.java
@@ -0,0 +1,13 @@
+package klauer.fitnessemvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
