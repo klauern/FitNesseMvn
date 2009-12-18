f9ac8268b28ece122940eeee38df8260e22bdb7a
Nick Klauer <klauer@gmail.com>
2009-12-17 22:01:14 -0600

    Working on utilizing JUnit for testing.  Pulled examples off of Trinidad website.

---
+++ src/test/java/klauer/fitnessemvn/JUnitHelperTrinidadTest.java
@@ -0,0 +1,31 @@
+package klauer.fitnessemvn;
+
+import java.io.File;
+import org.junit.Before;
+import org.junit.Test;
+import fitnesse.trinidad.*;
+
+public class JUnitHelperTrinidadTest {
+/*
+  JUnitHelper helper;
+
+  @Before
+  public void initHelper() throws Exception {
+    helper = new JUnitHelper(new TestRunner(
+            new FitNesseRepository("src/main/fitnesse"),
+            new FitTestEngine(),
+            new File(System.getProperty("java.io.tmpdir"), "fitnesse").getAbsolutePath()));
+  }
+
+  @Test
+  public void runSuite() throws Exception {
+    helper.assertSuitePasses("JavaExamples");
+  }
+
+  @Test
+  public void runSingleTest() throws Exception {
+    helper.assertTestPasses("FitNesse.UserGuide.TwoMinuteExample");
+  }
+ * 
+ */
+}
