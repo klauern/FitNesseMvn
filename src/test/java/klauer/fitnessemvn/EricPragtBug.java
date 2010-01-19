105f34ae36bf1fb67631acd2f1702593227fad42
Nick Klauer (a03182) <nicholasklauer@alliantenergy.com>
2010-01-19 14:48:00 -0600

    cleaned up pom, removed erroneous Fit pages

--- src/test/java/klauer/fitnessemvn/EricPragtBug.java
+++ src/test/java/klauer/fitnessemvn/EricPragtBug.java
@@ -9,16 +9,6 @@ import fitnesse.junit.FitNesseSuite.Name;
 import fitnesse.junit.FitNesseSuite.OutputDir;
 
 
-/**
- * This is a simple example of running a FitNesse Suite or Test from JUnit.  This
- * also allows you to test FitNesse pages from your IDE of choice (Eclipse or
- * NetBeans).
- *
- * To test a different suite of tests, replace the name of the Suite page under
- * @Name with your own.  Leave the rest as defaults for this.
- * 
- * @author A03182
- */
 @RunWith(FitNesseSuite.class)
 @Name("FitNesse.SuiteAcceptanceTests.SuiteSlimTests.ErikPragtBug")
 @FitnesseDir("src/main/fitnesse")
